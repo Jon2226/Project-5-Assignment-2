f08c015ceb1cfc49fd68bf695ae0fb9ba6994906
Jon Ward <jcmward@gmail.com>
2024-09-18 13:33:24 -0400

    Refactor to read emails as well as names from file
    Specifically, the read_names() function now uses a helper function
    read_line() to tokenize each line of the file and assign an email to
    the STUDENT_DATA struct only if it is found.
    
    Add pre-release compiler directive, currently used to select data file
    with emails instead of the names file.

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,7 +1,7 @@
 // CSCN73030-F24: Project 5 - Assignment 2
 // Jonathan Ward
 
-// Read student data from files and store in vector
+// Read student data from plaintext files and store in vector
 
 #include <algorithm>
 #include <iostream>
@@ -14,9 +14,12 @@ using namespace std;
 #define NAMES_FILE		"StudentData.txt"
 #define EMAILS_FILE		"StudentData_Emails.txt"
 
+#define PRE_RELEASE
+
 struct STUDENT_DATA {
 	string first_name;
 	string last_name;
+	string email;
 };
 
 // Remove leading or trailing whitespace from a string
@@ -25,6 +28,30 @@ string trim(const string& str) {
 	if (first == string::npos) return "";					// If all whitespace, return empty
 	size_t last = str.find_last_not_of(" \t\n\r\f\v");		// Find index of last non-whitespace
 	return str.substr(first, (last - first + 1));			// return string between those indices
+}
+
+// Each line should resemble this CSV format:
+// Lastname, Firstname, email@domain.com
+// However, email may or may not be present.
+STUDENT_DATA read_line(string line) {
+	STUDENT_DATA student;
+	istringstream ss(line);
+	string token;
+	vector<string> tokens;
+
+	while (getline(ss, token, ',')) {
+		tokens.push_back(trim(token));
+	}
+
+	// Only assign email if the correct number of string tokens are present
+	if (tokens.size() > 0)
+		student.last_name = tokens[0];
+	if (tokens.size() > 1)
+		student.first_name = tokens[1];
+	if (tokens.size() > 2)
+		student.email = tokens[2];
+
+	return student;
 }
 
 // Read student names from given plaintext data file into a vector
@@ -40,15 +67,8 @@ vector<STUDENT_DATA> read_names(string file_name) {
 	else
 	{
 		while (getline(input_file, line)) {
-			istringstream ss(line);
-			STUDENT_DATA student;
-			if (getline(ss, student.last_name, ',') && getline(ss, student.first_name)) {
-				// Remove any leading or trailing whitespace from each name
-				student.last_name = trim(student.last_name);
-				student.first_name = trim(student.first_name);
-
-				all_students.push_back(student);
-			}
+			STUDENT_DATA student = read_line(line);
+			all_students.push_back(student);
 		}
 	}
 	input_file.close();
@@ -56,16 +76,23 @@ vector<STUDENT_DATA> read_names(string file_name) {
 };
 
 int main() {
-    cout << "Running pre-release version" << endl;
+	cout << "Running pre-release version" << endl;
 
+#ifdef PRE_RELEASE
+	vector<STUDENT_DATA> all_students = read_names(EMAILS_FILE);
+ #else
 	vector<STUDENT_DATA> all_students = read_names(NAMES_FILE);
+ #endif
 
-// Using NDEBUG because it's standardized for C/C++, not relying on Visual Studio.
+// Using NDEBUG because it's standardized for C/C++ (doesn't specifically depend on Visual Studio).
 // See: https://github.com/microsoft/qsharp-runtime/issues/600
 #if !defined(NDEBUG)
 	// DEBUG: print all student information to stdout
 	for (const auto& student : all_students) {
-		cout << student.last_name << ", " << student.first_name << endl;
+		cout << "Last name: " << student.last_name << endl;
+		cout << "First name: " << student.first_name << endl;
+		cout << "Email: " << student.email << endl;
+		cout << endl;
 	}
 #endif
 
